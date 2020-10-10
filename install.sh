77bf75383202e6888ce4c1e2580f09ecc00d2166
ayeks <lars.luehr@ayeks.de>
2020-10-10 12:45:58 +0200

    chore: add default values.yaml

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@ helm repo add traefik https://helm.traefik.io/traefik
 helm repo update
 helm install \
     --set="additionalArguments={--log.level=DEBUG}" \
-    traefik traefik/traefik
+    traefik traefik/traefik \
+    -f values.yaml
 kubectl apply -f dashboard.yaml
